06ba9519613ee155c94bbffd40a6ee756414425f
Jakob Gruber <jakob.gruber@gmail.com>
2010-12-19 13:04:29 +0100

    open() implemented

--- bonus/sv_data.c
+++ bonus/sv_data.c
@@ -4,21 +4,26 @@
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 #include <linux/semaphore.h>
 #include <linux/slab.h>
 
 #include "common.h"
 
 typedef struct {
-        int major;
-        int minor;
-        struct cdev cdev;
-        struct semaphore sem;
         char *data;
         size_t size;
         char key[KEY_LEN];
         uid_t uid;
         int ready;
+} svd_data;
+
+typedef struct {
+        int major;
+        int minor;
+        struct cdev cdev;
+        struct semaphore sem;
+        svd_data contents;
 } svd_dev;
 
 static svd_dev devs[SV_NR_DEVS];
@@ -29,64 +34,73 @@ int svd_create(int id, uid_t uid, size_t size, const char *key) {
 
         if(size > (size_t)SV_MAX_SIZE) return(-ENOTSUPP);
 
-        if(down_interruptible(&devs[id].sem) != 0) return(-ERESTARTSYS);
+        if(down_interruptible(&dev->sem) != 0) return(-ERESTARTSYS);
         /* already initialized? */
-        if(dev->ready) {
+        if(dev->contents.ready) {
                 retval = -EEXIST;
                 goto exit;
         }
         /* alloc and zero mem */
-        dev->data = kmalloc(size, GFP_KERNEL);
-        if(dev->data == NULL) {
+        dev->contents.data = kmalloc(size, GFP_KERNEL);
+        if(dev->contents.data == NULL) {
                 retval = -ENOMEM;
                 goto exit;
         }
-        memset(dev->data, 0, size);
+        memset(dev->contents.data, 0, size);
         /* set properties */
-        dev->size = size;
-        dev->uid = uid;
-        strlcpy(dev->key, key, KEY_LEN);
-        dev->ready = 1;
+        dev->contents.size = size;
+        dev->contents.uid = uid;
+        strlcpy(dev->contents.key, key, KEY_LEN);
+        dev->contents.ready = 1;
 
 exit:
-        up(&devs[id].sem);
+        up(&dev->sem);
         return(retval);
 }
 int svd_truncate(int id, uid_t uid) {
         int retval = 0;
         svd_dev *dev = &devs[id];
 
-        if(down_interruptible(&devs[id].sem) != 0) return(-ERESTARTSYS);
-        if(dev->uid != uid) {
+        if(down_interruptible(&dev->sem) != 0) return(-ERESTARTSYS);
+        if(!dev->contents.ready) {
+                retval = -ENXIO;
+                goto exit;
+        }
+        if(dev->contents.uid != uid) {
                 retval = -EPERM;
                 goto exit;
         }
         /* zero region */
-        memset(dev->data, 0, dev->size);
+        memset(dev->contents.data, 0, dev->contents.size);
 
 exit:
-        up(&devs[id].sem);
+        up(&dev->sem);
         return(0);
 }
 int svd_remove(int id, uid_t uid) {
         int retval = 0;
         svd_dev *dev = &devs[id];
 
-        if(down_interruptible(&devs[id].sem) != 0) return(-ERESTARTSYS);
-        if(dev->uid != uid) {
+        if(down_interruptible(&dev->sem) != 0) return(-ERESTARTSYS);
+        if(!dev->contents.ready) {
+                retval = -ENXIO;
+                goto exit;
+        }
+        if(dev->contents.uid != uid) {
                 retval = -EPERM;
                 goto exit;
         }
         /* free dynamic mem and zero region */
-        kfree(dev->data);
-        memset(dev, 0, sizeof(svd_dev));
+        kfree(dev->contents.data);
+        memset(&dev->contents, 0, sizeof(svd_data));
 
 exit:
-        up(&devs[id].sem);
+        up(&dev->sem);
         return(0);
 }
 
 loff_t svd_llseek(struct file *filp, loff_t off, int whence) {
+
         return (loff_t)0;
 }
 ssize_t svd_write(struct file *filp, const char __user *buf, size_t count,
@@ -101,7 +115,27 @@ int svd_release(struct inode *inode, struct file *filp) {
         return(0);
 }
 int svd_open(struct inode *inode, struct file *filp) {
-        return(0);
+        int retval = 0;
+        svd_dev *dev;
+        dev = container_of(inode->i_cdev, svd_dev, cdev);
+        filp->private_data = dev;
+
+        if (down_interruptible(&dev->sem))
+                return(-ERESTARTSYS);
+
+        /* check if device is ready to use, */
+        if(!dev->contents.ready) retval = -ENXIO;
+
+        /* and if the current user has access */
+        else if(dev->contents.uid != current_uid()) retval = -EPERM;
+
+        /* now trim to 0 the length of the device if open was write-only */
+        else if((filp->f_flags & O_ACCMODE) == O_WRONLY)
+                memset(dev->contents.data, 0, dev->contents.size);
+
+        up(&dev->sem);
+
+        return(retval);
 }
 
 static struct file_operations svc_fops = {
@@ -134,7 +168,7 @@ int __init svd_setup(int id, int major) {
         return(0);
 }
 void __exit svd_remove_dev(int id) {
-        kfree(devs[id].data);
+        kfree(devs[id].contents.data);
         cdev_del(&devs[id].cdev);
         PDEBUG("removed sv_data%d\n", id);
 }
